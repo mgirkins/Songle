1e2a991137ce21d1965a821ad845fd33e2b41037
Max Girkins <mgirkins@gmail.com>
2017-12-03 20:26:25 +0000

    added a custom launcher icon and changed help screen copy to include description of map markers.

--- app/src/main/java/com/example/maxgirkins/songle/MainActivity.java
+++ app/src/main/java/com/example/maxgirkins/songle/MainActivity.java
@@ -364,7 +364,7 @@ public class MainActivity extends AppCompatActivity
         }
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
+        return false;
     }
 
     public void onLyricsDownloaded() throws InterruptedException {
